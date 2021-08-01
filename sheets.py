3f301a749190beed82a823dedb1627a577ff31db
PRASANTA KUMAR PARIDA <prasanta.parida@thinktalentindia.com>
2021-08-01 11:54:24 +0530

    google sheet api works

---
+++ sheets.py
@@ -0,0 +1,27 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json",scope)
+client= gspread.authorize(creds)
+sheet=client.open("Google_Sheet_API_Test").sheet1
+
+# Get All Sheet Data
+data=sheet.get_all_records()
+pprint(data)
+
+# Get Row Data At Particular Row
+row=sheet.row_values(3)
+# pprint(row)
+
+# Get Col Data At Particular Col
+col=sheet.col_values(3)
+# pprint(col)
+
+# insertRow=["Hello",5,"Red","Blue"]
+# sheet.update_cell(2,2,"CHANGED")
+
+
+
